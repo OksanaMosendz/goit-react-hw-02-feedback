5f9e00464b35592e2f0bf5067ccf668eca52bf64
OksanaMosendz <bagheera03@gmail.com>
2021-09-03 18:06:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,52 @@ import React from 'react';
 
 export class App extends React.Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: 5,
+    neutral: 7,
+    bad: 7,
+  };
+
+  addFeedBack = e => {
+    console.log(e);
+    console.log(e.target);
+    this.setState(prevState => ({
+      // switch(e.target):
+      // case :
+
+      good: prevState.good + 1,
+    }));
   };
 
   render() {
+    const { good, neutral, bad } = this.state;
+
     return (
       <>
         <h1>Please leave feedback</h1>
+        <div>
+          <button type="button" onClick={this.addFeedBack}>
+            Good
+          </button>
+          <button type="button" onClick={this.addFeedBack}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.addFeedBack}>
+            Bad
+          </button>
+        </div>
+
+        <h2>Statistics</h2>
+        <ul>
+          <li>
+            Good:<span>{good}</span>
+          </li>
+          <li>
+            Neutral:<span>{neutral}</span>
+          </li>
+          <li>
+            Bad:<span>{bad}</span>
+          </li>
+        </ul>
       </>
     );
   }
