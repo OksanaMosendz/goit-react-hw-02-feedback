4de67e395707c3da49718a985eb7ea82a5b2614e
OksanaMosendz <bagheera03@gmail.com>
2021-09-06 10:18:41 +0300

    add styles and props

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <ul>
@@ -18,4 +20,12 @@ export const Statistics = ({ good, neutral, bad, total, positivePercentage }) =>
       </li>
     </ul>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
